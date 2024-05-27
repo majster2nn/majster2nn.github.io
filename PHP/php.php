9590369f0d8ee01b728b60ea270de4ca9d94ab59
majster2nn <82648480+majster2nn@users.noreply.github.com>
2024-05-27 10:19:12 +0200

    Add files via upload

---
+++ PHP/php.php
@@ -0,0 +1,18 @@
+<html lang="pl">
+	<head>
+		<meta charset="utf-8">
+		<title>STRONA SZABLON</title>
+		<meta name="description" content="szkoła, lekcje, temat, nauka, html">
+		<meta name="keywords" content="szkoła, lekcje, temat, nauka, html">
+		<meta name="author" content="Stanisław Dereń">
+	</head>
+
+	<body>
+		<?php
+		// Skrypt ten wypisze tekst, uŻywając funkcji "print"
+		print("PHP");
+		print ("<br>");
+		print ("Zaczynam poznawać PHP");
+		?>
+	</body>
+</html>
